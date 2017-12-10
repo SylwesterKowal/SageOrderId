84481fd71794bfe2abc2b5154d3487a0f172342a
SK <sylwester.kowal@21w.pl>
2017-12-10 20:40:06 +0100

    Zmienne

--- Api/Data/CustomDataInterface.php
+++ Api/Data/CustomDataInterface.php
@@ -12,57 +12,14 @@ interface CustomDataInterface extends \Magento\Framework\Api\CustomAttributesDat
      * Constants defined for keys of the data array. Identical to the name of the getter in snake case
      */
     const ID = 'id';
-
-    const CUSTOMER_ID = 'customer_id';
-    const PRODUCT_ID = 'product_id';
-
     const SAGE_ORDER_ID = 'sage_order_id';
-    /**#@-*/
 
-    /**
-     * Get Id.
-     *
-     * @return int|null
-     */
     public function getId();
 
-    /**
-     * Set Id.
-     *
-     * @param int $id
-     * @return $this
-     */
     public function setId($id = null);
 
-    /**
-     * Get Customer Id.
-     *
-     * @return int|null
-     */
-    public function getCustomerId();
     public function getSageOrderId();
 
-    /**
-     * Set Customer Id.
-     *
-     * @param int $customerId
-     * @return $this
-     */
-    public function setCustomerId($customerId = null);
-    public function setSageOrderId($customerId = null);
+    public function setSageOrderId($SageOrderId = null);
 
-    /**
-     * Get Product Id.
-     *
-     * @return int|null
-     */
-    public function getProductId();
-
-    /**
-     * Set Product Id.
-     *
-     * @param int $productId
-     * @return $this
-     */
-    public function setProductId($productId = null);
 }
--- Model/Data/CustomData.php
+++ Model/Data/CustomData.php
@@ -12,77 +12,27 @@ class CustomData extends \Magento\Framework\Api\AbstractExtensibleObject impleme
     \Wm21w\Sage\Api\Data\CustomDataInterface
 {
 
-    /**
-     * Get Id.
-     *
-     * @return int|null
-     */
     public function getId()
     {
         return $this->_get(self::ID);
     }
 
-    /**
-     * Set Id.
-     *
-     * @param int $id
-     * @return $this
-     */
     public function setId($id = null)
     {
         return $this->setData(self::ID, $id);
     }
 
-    /**
-     * Get Customer Id.
-     *
-     * @return int|null
-     */
-    public function getCustomerId()
-    {
-        return $this->_get(self::CUSTOMER_ID);
-    }
 
     public function getSageOrderId()
     {
         return $this->_get(self::SAGE_ORDER_ID);
     }
 
-    /**
-     * Set Customer Id.
-     *
-     * @param int $customerId
-     * @return $this
-     */
-    public function setCustomerId($customerId = null)
-    {
-        return $this->setData(self::CUSTOMER_ID, $customerId);
-    }
 
     public function setSageOrderId($sageOrderId = null)
     {
         return $this->setData(self::SAGE_ORDER_ID, $sageOrderId);
     }
 
-    /**
-     * Get Product Id.
-     *
-     * @return int|null
-     */
-    public function getProductId()
-    {
-        return $this->_get(self::PRODUCT_ID);
-    }
-
-    /**
-     * Set Product Id.
-     *
-     * @param int $productId
-     * @return $this
-     */
-    public function setProductId($productId = null)
-    {
-        return $this->setData(self::PRODUCT_ID, $productId);
-    }
 
 }
