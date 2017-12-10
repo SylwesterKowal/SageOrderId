c505079ec4a5b7fbb55d0c9d5c4ae68c4b9c5dde
SK <sylwester.kowal@21w.pl>
2017-12-11 00:13:26 +0100

    SageOrderID display

--- Block/Adminhtml/Order/View/Custom.php
+++ Block/Adminhtml/Order/View/Custom.php
@@ -3,5 +3,8 @@
 namespace Wm21w\Sage\Block\Adminhtml\Order\View;
 class Custom extends \Magento\Backend\Block\Template
 {
-
+    public function getSageOrderID()
+    {
+        return 'Hello world';
+    }
 }
